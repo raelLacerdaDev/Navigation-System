d34339d79a9698c11e62fbd42b0fec84906a250a
raelLacerdaDev <raellacerda.developer@gmail.com>
2025-10-21 10:50:29 -0300

    adding 'NavigationSystem.kt'

---
+++ NavigationSystem.kt
@@ -0,0 +1,103 @@
+fun main() {
+    println("first input:")
+    testeNavigation(
+        browser = Browser(),
+        commands = arrayOf(
+            "get-current",
+            "access,https://amazon.com",
+            "access,https://cnn.com",
+            "get-current",
+            "back",
+            "get-current",
+            "back",
+            "get-current",
+            "back"
+        )
+    )
+    println()
+    println("second input:")
+    testeNavigation(
+        browser = Browser(),
+        commands = arrayOf(
+            "access,https://amazon.com",
+            "access,https://cnn.com",
+            "get-current",
+            "forward"
+        )
+    )
+    println()
+    println("third input:")
+    testeNavigation(
+        browser = Browser(),
+        commands = arrayOf(
+            "access,https://amazon.com",
+            "access,https://cnn.com",
+            "access,https://gmail.com",
+            "access,https://outlook.com",
+            "get-current",
+            "back",
+            "back",
+            "back",
+            "get-current",
+            "forward",
+            "forward",
+            "get-current"
+        )
+    )
+    println()
+    println("fourth input:")
+    testeNavigation(
+        browser = Browser(),
+        commands = arrayOf(
+            "access,https://amazon.com",
+            "access,https://cnn.com",
+            "access,https://gmail.com",
+            "access,https://outlook.com",
+            "get-current",
+            "back",
+            "back",
+            "back",
+            "get-current",
+            "forward",
+            "forward",
+            "get-current",
+            "access,https://devsuperior.com.br",
+            "back",
+            "forward",
+            "get-current",
+            "forward"
+        )
+    )
+
+}
+
+fun testeNavigation (browser: Browser, commands: Array<String>) {
+    commands.forEach { command ->
+        when {
+            command == "get-current" -> {
+                println(browser.getCurrentPage())
+            }
+            command == "back" -> {
+                try {
+                    browser.back()
+                } catch (e: Error) {
+                    println("Error: ${e.message}")
+                }
+            }
+            command == "forward" -> {
+                try {
+                    browser.forward()
+                } catch (e: Error) {
+                    println("Error: ${e.message}")
+                }
+            }
+            command.startsWith("access") -> {
+                val newArr = command.split(",")
+                browser.access(newArr[1])
+            }
+            else -> {
+                println("Invalid Command: $command")
+            }
+        }
+    }
+}
